fdb4bdf042b6f5f54155f3c92b6a62ab7c8fd427
viennetta <darianjohnblanchard@gmail.com>
2025-02-13 20:14:10 -0500

    Initial commit

---
+++ src/main/java/net/viennetta/harukablack/HarukaBlackClient.java
@@ -0,0 +1,10 @@
+package net.viennetta.harukablack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HarukaBlackClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
